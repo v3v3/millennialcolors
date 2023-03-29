a49de0ccded6811ec494839f4b1374a1af5c75db
v3v3 <yiti.essos@gmail.com>
2023-03-29 08:34:26 +0800

    First commit

---
+++ colors.js
@@ -0,0 +1,183 @@
+const colors = [
+  {
+    "name": "Millennial Pink",
+    "hex": "#EFB6BF"
+  }, {
+    "name": "Polished Copper",
+    "hex": "#C86E4C"
+  }, {
+    "name": "Quartz",
+    "hex": "#DDDDDD"
+  }, {
+    "name": "One Million Likes",
+    "hex": "#FE3C71"
+  }, {
+    "name": "Succulent",
+    "hex": "#2BD566"
+  }, {
+    "name": "Social Bubble",
+    "hex": "#0081FE"
+  }, {
+    "name": "Paris Hilton Pink",
+    "hex": "#DF19C1"
+  }, {
+    "name": "Nickelodeon Splat",
+    "hex": "#FF6200"
+  }, {
+    "name": "Little Miss Sunshine",
+    "hex": "#EFC40E"
+  }, {
+    "name": "Limewire",
+    "hex": "#23D513"
+  }, {
+    "name": "Yes We Cyan",
+    "hex": "#4399DE"
+  }, {
+    "name": "Zenon Ruby",
+    "hex": "#E33056"
+  }, {
+    "name": "Oprah's Couch",
+    "hex": "#B13A1A"
+  }, {
+    "name": "Tracksuit",
+    "hex": "#832C76"
+  }, {
+    "name": "Da ba dee da ba daa",
+    "hex": "#164DB0"
+  }, {
+    "name": "Seattle Spruce", 
+    "hex": "#287E9E"
+  }, {
+    "name": "DOH DIJON",
+    "hex": "#E4A834"
+  }, {
+    "name": "Moody Mauve",
+    "hex": "#B3346C"
+  }, {
+    "name": "Acid Wash",
+    "hex": "#3968CB"
+  }, {
+    "name": "Purple Rain",
+    "hex": "#CA7CD8"
+  }, {
+    "name": "Tron Turquoise",
+    "hex": "#10E7E2"
+  }, {
+    "name": "Miami",
+    "hex": "#FF68A8"
+  }, {
+    "name": "PACMAN",
+    "hex": "#F9EB0F"
+  }, {
+    "name": "Powersuit",
+    "hex": "#FF2153"
+  }, {
+    "name": "Prom Suit",
+    "hex": "#00A1D3"
+  }, {
+    "name": "Wonka Kitsch",
+    "hex": "#A92DA3"
+  }, {
+    "name": "Lava Lamp Glo",
+    "hex": "#FD4D2E"
+  }, {
+    "name": "Faux Fern",
+    "hex": "#769F52"
+  }, {
+    "name": "Neon Dreams",
+    "hex": "#FF68A8"
+  }, {
+    "name": "Have a Nice Day",
+    "hex": "#F8CA38"
+  }, {
+    "name": "Burnt Sienna",
+    "hex": "#CF4917"
+  }, {
+    "name": "Harvest Gold",
+    "hex": "#F9AC3D"
+  }, {
+    "name": "Avocado",
+    "hex": "#758C33"
+  }, {
+    "name": "Teak",
+    "hex": "#985914"
+  }, {
+    "name": "Natural",
+    "hex": "#D0B285"
+  }, {
+    "name": "Blue Mustang",
+    "hex": "#2D758C"
+  }, {
+    "name": "Poodle Skirt",
+    "hex": "#FF91BB"
+  }, {
+    "name": "Lemon Meringue",
+    "hex": "#FFD95C"
+  }, {
+    "name": "Roadtrip",
+    "hex": "#4AC6D7"
+  }, {
+    "name": "Motel Sunset",
+    "hex": "#F5855B"
+  }, {
+    "name": "Soda Fountain",
+    "hex": "#68BBB8"
+  }, {
+    "name": "Marilyn in Crimson",
+    "hex": "#E81B23"
+  }, {
+    "name": "War Bond Blue",
+    "hex": "#2A326D"
+  }, {
+    "name": "Keep Calm & Canary On",
+    "hex": "#F4CF0D"
+  }, {
+    "name": "Rosie's Nail Polish",
+    "hex": "#D90707"
+  }, {
+    "name": "Air Force Blue",
+    "hex": "#4E9FBC"
+  }, {
+    "name": "Normandy Sand",
+    "hex": "#DABD8F"
+  }, {
+    "name": "Cadet Khaki",
+    "hex": "#B38069"
+  }, {
+    "name": "Poppy Field",
+    "hex": "#D02D1C"
+  }, {
+    "name": "Yellow Brick Road",
+    "hex": "#EED023"
+  }, {
+    "name": "Mint",
+    "hex": "#B0DABE"
+  }, {
+    "name": "Powder Blue",
+    "hex": "#99B1C9"
+  }, {
+    "name": "Egyptian Blue",
+    "hex": "#345D98"
+  }, {
+    "name": "Jade",
+    "hex": "#3A7359"
+  }, {
+    "name": "Tuscan Sun",
+    "hex": "#F4B308"
+  }, {
+    "name": "Antique Gold",
+    "hex": "#AB933C"
+  }, {
+    "name": "Champagne",
+    "hex": "#E5CE9C"
+  }, {
+    "name": "Cadmium Red",
+    "hex": "#CA2222"
+  }, {
+    "name": "Ultramarine",
+    "hex": "#4D51A9"
+  }, {
+    "name": "Deco Silver",
+    "hex": "#A5AAA0"
+  }
+]
